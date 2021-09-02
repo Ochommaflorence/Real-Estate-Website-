c3ec826eec4e239991f7e1d9f24d86b36a7c081c
chizzy01 <elvischizzy09@gmail.com>
2021-09-02 17:25:58 +0100

    files are been committed in hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled, { css } from "styled-components/macro";
 import { Button } from "./Button";
-import { IoMdArrowRoundForward } from 'react-icons/io';
-import { IoArrowForward, IoArrowBackward } from 'react-icons/io5';
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { IoArrowForward, IoArrowBackward } from "react-icons/io5";
 
 const HeroSection = styled.section`
   height: 100vh;
@@ -20,13 +20,24 @@ const HeroWrapper = styled.div`
   position: relative;
 `;
 
-const HeroSlide = styled.div``
-const HeroSlider = styled.div``
-const HeroImage = styled.img``
-const HeroContent = styled.div``
+const HeroSlide = styled.div``;
+const HeroSlider = styled.div``;
+const HeroImage = styled.img``;
+const HeroContent = styled.div``;
 const Arrow = styled(IoMdArrowRoundForward)``;
 
-
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color:#fff;
+  cursor: pointer;
+  background:#000d1a;
+  border-radius:50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transition: 0.3s;
+`;
 
 const Hero = ({ slides }) => {
   return (
@@ -38,17 +49,27 @@ const Hero = ({ slides }) => {
               <HeroSlider>
                 <HeroImage />
                 <HeroContent>
-                    <h1>{slide.title}</h1>
-                    <p>{slide.price}</p>
-                    <Button to={slide.path} primary="true" css={`max-width: 160px`}>
-                        {slide.label}
-                        <Arrow/>
-                    </Button>
+                  <h1>{slide.title}</h1>
+                  <p>{slide.price}</p>
+                  <Button
+                    to={slide.path}
+                    primary="true"
+                    css={`
+                      max-width: 160px;
+                    `}
+                  >
+                    {slide.label}
+                    <Arrow />
+                  </Button>
                 </HeroContent>
               </HeroSlider>
             </HeroSlide>
           );
         })}
+        <SliderButtons>
+            <PrevArrow/>
+            <NextArrow/>
+        </SliderButtons>
       </HeroWrapper>
     </HeroSection>
   );
